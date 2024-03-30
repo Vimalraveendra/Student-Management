d311e8e9af6831ffcd954e77e2c7cc4eda8dca42
Vimal Raveendran <vimalraveendra@gmail.com>
2024-03-30 20:22:03 +0100

    first commit

---
+++ src/main/java/com/example/studentmanagementsystem/entity/Student.java
@@ -0,0 +1,19 @@
+package com.example.studentmanagementsystem.entity;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Student {
+    @Id
+    @GeneratedValue
+    private Long studentId;
+    private String firstName;
+    private String lastName;
+    private String email;
+}
